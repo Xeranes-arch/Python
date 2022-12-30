ddc95f5734873242f6bcc135cd42e5056d32d4cf
Xeranes-arch <florianklee42@gmail.com>
2022-12-30 12:42:24 +0100

    Conway's game of life 1.0

--- Test.py
+++ Test.py
@@ -1,17 +1,14 @@
 import numpy as np
 from numpy import loadtxt
 
+
 shape = (5,4)
 world = np.random.randint(0,2,shape)
-
-
 print(world)
 
-print("Give Filename:")
-seed = input("seed_")
-np.savetxt((seed), world)
-
-file = open(seed)
-world = loadtxt(file)
-
-print(world)
+world_p = world
+world_p = world_p.astype(str)
+world_p = np.char.replace(world_p,'1',(u'\u2588\u2588'))
+world_p = np.char.replace(world_p,'0',(u'\u0020\u0020'))
+world_p = np.array2string(world_p, separator='', formatter={'str_kind': lambda world_p: world_p})
+print(world_p)
--- cgol/cgol.py
+++ cgol/cgol.py
@@ -14,33 +14,55 @@ The initial pattern constitutes the seed of the system. The first generation is 
 
 - Wikipedia on Conway's game of life [adapted for p4p]
 """
+
+# Programm does not catch anything but a keyboard interupt to save the game
+
 import time
 import numpy as np
 from numpy import loadtxt
 
 def init_world(shape):
-    """Creates World"""
+    """
+    Initializes 2dim world of random states 0 or 1
+    
+    Args: 
+        shape (tuple): the dimensions of the world
+
+    Returns:
+        (np.array): initialized random world
+    """
     world = np.random.randint(0,2,shape)
     return world
 
 
 def neighbours(world):
-    """Gives Number of live neighbours"""
+    """
+    Gives Array of the number of living cell neighbours
+    
+    Args:
+        world (np.array): 
+
+    Returns:
+        alive_neighbours (np.array):
+    """
     count = 0
     alive_neighbours = np.zeros((world.shape[0],world.shape[1]),dtype=int)
+    #loop over all indizes x,y of world
     for y in range(world.shape[0]):
         for x in range(world.shape[1]):
-            #surrounding von x,y
+            #bounds of 3x3 grid around current cell
             x_0 = x - 1
             x_1 = x + 1
             y_0 = y - 1
             y_1 = y + 1
+            #loop over grid, count live neighbours
             for j in range(y_0, y_1+1):
                 for i in range(x_0, x_1+1):
+                    #making sure world borders aren't crossed
                     if i < 0 or i > world.shape[1]-1 or j < 0 or j > world.shape[0]-1:
                         continue
                     count = count + world[j,i]
-            # "- world[y,x]" weil summe über alle neun im 3x3 statt nur surrounding
+            #write alive_neighbours, "- world[y,x]" to subtract cells own value from 3x3 grid
             alive_neighbours[y,x] = count - world[y,x]
             count = 0
     return alive_neighbours
@@ -67,18 +89,27 @@ def update(world):
     new_world = neighbours(world)
     for y in range(world.shape[0]):
         for x in range(world.shape[1]):
+            #updating live cells
             if world[y,x] == 1 and (new_world[y,x] < 2 or new_world[y,x] > 3):
                 new_world[y,x] = 0
+            #updating dead cells
             elif world[y,x] == 0 and new_world[y,x] == 3:
                 new_world[y,x] = 1
+            #rest stays the same
             else:
                 new_world[y,x] = world[y,x] 
-    # Write your method here and make sure the return type matches the one
-    # given in the docstring.
     return new_world
 
 
-def _run(world, cycles=1):
+def process_output(world):
+    """Make output pretty"""
+    world_p = (world.astype(int)).astype(str)
+    world_p = np.array2string(world_p, separator='', formatter={'str_kind': lambda world_p: world_p})
+    world_p = np.char.replace(world_p,'1',(u'\u2588\u2588'))
+    world_p = np.char.replace(world_p,'0',(u'\u0020\u0020'))
+    return(world_p)
+
+def _run(world, cycles=0.5):
     """update the world <cycles> number of times
 
     Args:
@@ -89,16 +120,27 @@ def _run(world, cycles=1):
     Returns:
         new_world (np.array): the state of the world after <cycles> updates.
     """
-    # Write your method here and make sure the return type matches the one
-    # given in the docstring.
     start_time = time.time()
-    interval = 0.5
-    for i in range(1,3):
-        time.sleep(start_time + i*interval - time.time())
-        for j in range(cycles):
+    interval = 1/cycles
+    #120 cycles at intended 2Hz, max runtime of 1Minute
+    for i in range(1,121):
+        try:
+            time.sleep(start_time + i*interval - time.time())
+            world_old = world
             world = update(world)
+            world_p = process_output(world)
+            print(world_p)
             print(world)
-            j = j+1
+            i=i+1
+            #Escape when world gets stuck in repeating pattern
+            if np.array_equal(world, world_old):
+                print("Stable world state.")
+                escape(world)
+        except KeyboardInterrupt:
+            #Save after interrupt
+            print("\nProgramm interupted by user.")
+            print(world)
+            escape(world)
     return world
 
 
@@ -111,14 +153,14 @@ def load(filename):
     Returns:
         (np.array): a CGoL world
     """
-    print(filename)
+    print("Opening file\n" + filename)
     file = open(filename)
     world = loadtxt(file, delimiter = ",")
     return world
 
 
 def save(filename, world):
-    """Save pattern to filename as csv (Hint: Pay attention to the delimiter that separates values. Here, we want to use a comma (,)).
+    """Save world to filename as csv.
 
     Args:
         filename (str): the path to save the world to.
@@ -127,37 +169,45 @@ def save(filename, world):
     np.savetxt(filename, world, delimiter=",")
 
 
-def main():
-    """the game
+def escape(world):
+    print("Input seed to save current game state under.")
+    filename = (r"C:\Users\sabse\Documents\Python\cgol\seeds\seed_" + input("seed_") + ".csv")
+    #Programm doesn't save when no seed is given
+    if not filename == r"C:\Users\sabse\Documents\Python\cgol\seeds\seed_.csv":
+        save(filename, world)
+    print("Programm is exiting.")
+    #The End
+    exit()
 
-    Here, everything is assembled.
+
+def main():
+    """
     A random world is initialised by passing a shape or an existing world is loaded by passing a filename,
     and runs at 2 iterations per second until a KeyboardInterrupt is risen.
     It then queries a filename to save to and leaves.
     """
     #Choose mode
-    print("(1) Random game \n(2) Load file")
+    print("Conway's game of life\nChoose a mode:\n(1) Random game \n(2) Load file")
     mode = int(input())
+    #Load from seed
     if (mode - 1):
-        print("Give seed:")
-        filename = (r"C:\Users\sabse\Documents\Python\seeds\seed_" + input("seed_") + ".csv")
+        print("Input seed you wish to load:")
+        filename = (r"C:\Users\sabse\Documents\Python\cgol\seeds\seed_" + input("seed_") + ".csv")
         world = load(filename)
+    #Generate random world
     elif not (mode - 1):
-        x = int(input("Give shape: \nx="))
+        x = int(input("Set shape of the world: \nx="))
         y = int(input("y="))
-        shape = (x,y)
+        shape = (y,x)
         world = init_world(shape)
-        print(world)
+    world_p = process_output(world)
+    #Initial world state
+    print("This is the initial state of the world\n",world_p,"\nrunning cgol")
+    time.sleep(3)
 
-    #Run
-    world = _run(world)
-    print(world)
+    #Run it
+    _run(world)
     
-    #TODO Catch Keyboard interupt
-    
-    #Save
-    filename = (r"C:\Users\sabse\Documents\Python\seeds\seed_" + input("seed_") + ".csv")
-    save(filename, world)
 
 
 if __name__ == "__main__":
