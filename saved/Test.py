332bcda1dfef698305909fe3389b5d8f9a24e3c5
Xeranes-arch <florianklee42@gmail.com>
2023-01-10 23:09:27 +0100

    structure

--- saved/Test.py
+++ saved/Test.py
@@ -2,6 +2,6 @@ import numpy as np
 from numpy import loadtxt
 import json
 
-file = open('C:\\Users\\Xeonis7\\Documents\\Python\\saved\\a.json')
+file = open('C:\\Users\\Xeonis7\\Documents\\Python\\a.json')
 halloffame = json.load(file)
 print(halloffame)
